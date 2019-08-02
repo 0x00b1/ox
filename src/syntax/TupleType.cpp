5888d4e719c1b66428cd55ebad9317727c1f510d
Allen Goodman <allen.goodman@icloud.com>
2019-08-02 10:40:40 -0400

    TupleType

---
+++ src/syntax/TupleType.cpp
@@ -0,0 +1,11 @@
+#include "TupleType.hh"
+
+Node::TupleType::TupleType(std::vector<std::shared_ptr<Type>> types) {
+  this->types = std::move(types);
+};
+
+void Node::TupleType::accept(Visitor &visitor) {
+  std::shared_ptr<TupleType> p{shared_from_this()};
+
+  visitor.accept(p);
+}
