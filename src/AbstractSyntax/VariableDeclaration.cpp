5fb05796e76f3ebe2c26fa3bf87a2ba39fc4f3ca
Allen Goodman <allen.goodman@icloud.com>
2019-08-20 10:45:36 -0400

    VariableDeclaration

--- src/AbstractSyntax/VariableDeclaration.cpp
+++ src/AbstractSyntax/VariableDeclaration.cpp
@@ -1,9 +1,15 @@
 #include "VariableDeclaration.h"
 
-Node::VariableDeclaration::VariableDeclaration() {
+Node::VariableDeclaration::VariableDeclaration(std::shared_ptr<Pattern> pattern, std::shared_ptr<Type> type, std::shared_ptr<Node> expression) {
+  this->pattern = std::move(pattern);
 
+  this->type = std::move(type);
+
+  this->expression = std::move(expression);
 }
 
 void Node::VariableDeclaration::accept(Visitor &visitor) {
+  std::shared_ptr<VariableDeclaration> p{shared_from_this()};
 
+  visitor.accept(p);
 }
--- src/AbstractSyntax/VariableDeclaration.h
+++ src/AbstractSyntax/VariableDeclaration.h
@@ -9,7 +9,13 @@
 
 class Node::VariableDeclaration: public Node, public std::enable_shared_from_this<VariableDeclaration> {
 public:
-  VariableDeclaration();
+  VariableDeclaration(std::shared_ptr<Pattern> pattern, std::shared_ptr<Type> type, std::shared_ptr<Node> expression);
+
+  std::shared_ptr<Pattern> pattern;
+
+  std::shared_ptr<Type> type;
+
+  std::shared_ptr<Node> expression;
 
   void accept(Visitor &visitor) override;
 };
